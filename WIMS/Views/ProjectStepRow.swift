ff6fc791d81abf7965211f3be9fe01cd94e21eeb
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2021-10-06 20:19:01 -0300

    ProjectStepRow without Model

--- WIMS/Views/ProjectStepRow.swift
+++ WIMS/Views/ProjectStepRow.swift
@@ -5,4 +5,39 @@
 //  Created by Thiago Medeiros on 06/10/21.
 //
 
-import Foundation
+import SwiftUI
+
+struct ProjectStepRow: View {
+    var stepTitle: String
+    var isDone: Bool
+    
+    var body: some View {
+        HStack(spacing: 16) {
+            Circle()
+                .fill(isDone ? .black : .white)
+                .frame(width: 24, height: 24)
+                .wimsShadow()
+            
+            HStack(spacing: 8) {
+                // MARK: TODO change to step image
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(Color(UIColor.systemGray3))
+                    .frame(width: 80, height: 80)
+                
+                Text(stepTitle)
+                    .font(.title3)
+            }
+            .background(Color(UIColor.systemBackground))
+            .cornerRadius(8)
+        }
+        .frame(maxHeight: 80)
+    }
+}
+
+struct ProjectStepRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ProjectStepRow(stepTitle: "Nome do passo super top em até 2 linhas", isDone: true)
+        ProjectStepRow(stepTitle: "Nome do passo super top em até 2 linhas", isDone: false)
+    }
+}
+
