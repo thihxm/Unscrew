34e59b737d4d257480b66e47dfc8907b97fe1db1
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2021-10-06 22:28:46 -0300

    SearchBar without binding

---
+++ WIMS/Views/SearchBar.swift
@@ -0,0 +1,63 @@
+//
+//  SearchBar.swift
+//  WIMS
+//
+//  Created by Thiago Medeiros on 06/10/21.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    @State var text: String = ""
+    
+    @available(iOS 15.0, *)
+    @FocusState private var isTyping: Bool
+    
+    var search: () -> Void = {}
+    
+    var body: some View {
+        HStack {
+            if #available(iOS 15.0, *) {
+                TextField("Placeholder", text: $text)
+                    .frame(height: 32)
+                    .submitLabel(.search)
+                    .onSubmit {
+                        search()
+                    }
+                    .focused($isTyping)
+            } else {
+                TextField("Placeholder", text: $text, onCommit: search)
+                    .frame(height: 32)
+            }
+            
+            Button(action: {
+                if #available(iOS 15.0, *) {
+                    isTyping = false
+                } else {
+                    // Dismiss keyboard prior to iOS 15
+                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                }
+                
+                search()
+            }) {
+                Image(systemName: "magnifyingglass")
+                    .frame(width: 32, height: 32)
+                    .foregroundColor(.white)
+                    .background(Color.black)
+                    .clipShape(Circle())
+            }
+            .buttonStyle(PlainButtonStyle())
+        }
+        .padding(.horizontal, 16)
+        .padding(.vertical, 8)
+        .background(Color.white)
+        .cornerRadius(16)
+        .wimsShadow()
+    }
+}
+
+struct SearchBar_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBar()
+    }
+}
