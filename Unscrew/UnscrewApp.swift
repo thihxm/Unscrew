8baf2ea27358c4613f1ddb7ea51e27daf0524d20
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2021-10-13 13:56:00 -0300

    Use default persistent controller

--- Unscrew/UnscrewApp.swift
+++ Unscrew/UnscrewApp.swift
@@ -16,7 +16,7 @@ struct UnscrewApp: App {
     var body: some Scene {
         WindowGroup {
             HomeProjectsView()
-                .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
                 .preferredColorScheme(.light)
         }
         .onChange(of: scenePhase) { phase in
